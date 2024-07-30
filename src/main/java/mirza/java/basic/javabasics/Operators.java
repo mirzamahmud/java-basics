7b04e20f18dd8d1583d1bc3d68336b1cfd76ab8a
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-07-31 02:32:50 +0600

    only_working_on_unary_operators

---
+++ src/main/java/mirza/java/basic/javabasics/Operators.java
@@ -0,0 +1,56 @@
+package mirza.java.basic.javabasics;
+
+// Operator: it is a symbol that is used to perform operations.
+public class Operators {
+    // this method is used for unary operator
+    public static void exampleOfUnaryOperator(){
+        int firstNumber = 10;
+        int secondNumber = -20;     
+        boolean boolVar = true;
+        
+        System.out.println("Postfix (expr++): " + (firstNumber++)); // expr++
+        System.out.println("Postfix (expr--): " + (firstNumber--)); // expr--
+        System.out.println("Prefix (++expr): " + (++firstNumber)); // ++expr
+        System.out.println("Prefix (--expr): " + (--firstNumber)); // --expr
+        
+        System.out.println("First Number: " + (~firstNumber)); // minus of total positive value which starts from 0
+        System.out.println("Second Number: " + (~secondNumber)); // positive of total minus, positive starts from 0
+        
+        System.out.println("Boolean Var: " + (!boolVar)); // opposite of boolean value
+    }
+    
+    // this method is used for arithmetic operator
+    public static void exampleOfArithmeticOperator(){
+        
+    }
+    
+    // this method is used for shift operator
+    public static void exampleOfShiftOperator(){
+        
+    }
+    
+    // this method is used for relational operator
+    public static void exampleOfRelationalOperator(){
+        
+    }
+    
+    // this method is used for bitwise operator
+    public static void exampleOfBitwiseOperator(){
+        
+    }
+    
+    // this method is used for logical operator
+    public static void exampleOfLogicalOperator(){
+        
+    }
+    
+    // this method is used for ternary operator
+    public static void exampleOfTernaryOperator(){
+        
+    }
+    
+    // this method is used for assignment operator
+    public static void exampleOfAssignmentOperator(){
+        
+    }
+}
