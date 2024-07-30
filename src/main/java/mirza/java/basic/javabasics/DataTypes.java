84c390af9f19936dfe49310fca3405932d052751
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-07-30 20:48:45 +0600

    added_all_primitive_data_types

--- src/main/java/mirza/java/basic/javabasics/DataTypes.java
+++ src/main/java/mirza/java/basic/javabasics/DataTypes.java
@@ -13,6 +13,30 @@ public class DataTypes {
         // 2. Byte Data Type ---------- 8 bits / 1 byte (Range: -128 to 127)
         byte byteVar = 125;
         System.out.println("Byte: " + byteVar);
+        
+        // 3. Short Data Type --------- 16 bits / 2 bytes (Range: -32,768 to 32,767)
+        short shortVar = 32760;
+        System.out.println("Short: " + shortVar);
+        
+        // 4. Int Data Type --------- 32 bits / 4 bytes (Range: -2,147,483,648 (-2^31) to 2,147,483,647 (2^31 -1))
+        int intVar = 32760;
+        System.out.println("Int: " + intVar);
+        
+        // 5. Long Data Type --------- 64 bits / 8 bytes (Range: -9,223,372,036,854,775,808(-2^63) to 9,223,372,036,854,775,807(2^63 -1))
+        long longVar = 2589996632255555556L;
+        System.out.println("Long: " + longVar);
+        
+        // 6. Float Data Type --------- 32 bits / 4 bytes (Range: unlimited)
+        float floatVar = 2589.8565444445556662222222228996635f;
+        System.out.println("Float: " + floatVar);
+        
+        // 7. Double Data Type --------- 64 bits / 8 bytes (Range: unlimited)
+        double doubleVar = 2589.8565444445556662222222228996635d;
+        System.out.println("Double: " + doubleVar);
+        
+        // 8. Char Data Type --------- 16 bits / 2 bytes (Range: '\u0000' (or 0) to '\uffff' (or 65,535 inclusive))
+        char charVar = '\u5500';
+        System.out.println("Char: " + charVar);
     }
     
     // this method is used to define non-primitive data types.
